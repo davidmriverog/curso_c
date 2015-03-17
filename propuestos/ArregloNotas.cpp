f163b0e56a119e2771675acf391643a5f60e3d17
David Rivero <davidmriverog@gmail.com>
2015-03-17 08:22:33 -0430

    Creamos los Controles de Flujos y Arrays
    se comienza a implementar en el proyecto el uso de controles de flujos y
    un condicional que valida la cantidad de alumnos a ingresar mediante el
    limite establecido a 50.

--- propuestos/ArregloNotas.cpp
+++ propuestos/ArregloNotas.cpp
@@ -7,42 +7,77 @@
 #include <stdlib.h>
 
 // --> Constante o Limite Maximo
-#define max 3
+#define max 50
 
 // --> Atributos Generales
-struct{
-	char nombres[30];
-	float nota_alumno;
-	char tipo_nota;
-}alumnos[max];
-int opcion;
+
+int opcion,cedula[max],cant_alumnos;
 char respuesta='s';
+int nota[max];
 	
 // --> Funcion para capturar los datos por pantalla
 void entrada(){
     
-    while(respuesta=='s'||respuesta=='S'){
-    	
-	}
-	/* Menu Principal */
-    printf("1.- Ingresar Alumnos \n");
-    printf("2.- Ver Todos \n");
-    printf("3.- Ver Alumnos por Clasificacion de Notas");
-    printf("3.- Salir \n");
+	printf("Ingrese la Cantidad de Alumnos a Registrar:");
+    scanf("%d",&cant_alumnos);
     
-    printf("Eliga una Opcion:");
-    scanf("%d",&opcion);
+    // --> Validamos que la cantidad de alumnos a ingresar cumpla con el limite...
+    if(cant_alumnos>=0 && cant_alumnos<=max){
+	  
+	  while(respuesta=='s'||respuesta=='S'){
+      
+      /* Menu Principal */
+      system("cls");
+      printf("1).- Ingresar Alumnos \n");
+      printf("2).- Ver Todos \n");
+      printf("3).- Ver Alumnos por Clasificacion de Notas \n");
+      printf("4).- Salir \n");
+      
+      printf("Eliga una Opcion:");
+      scanf("%d",&opcion);
     
-    switch(opcion){
+      switch(opcion){
 		
-		case 1:
-			printf("Opcion 1 - Registrar Alumnos");
-			break;
+	    case 1:
+		   
+		   printf("Opcion 1 - Registrar Alumnos \n");
+		   
+		   for(int i=0;i<cant_alumnos;i++){
+		      printf("Ingrese Cedula:");
+			  scanf("%d",&cedula[i]);			  
+			}
+			printf("Desea Seguir (S/N):");
+			respuesta=getch();
 			
+		 break;
+	    case 2:
+			printf("Opcion 2 - Ver Todos \n");
+			
+			printf("Cedula \n");
+			for(int j=0;j<cant_alumnos;j++){
+				
+				printf("%d",cedula[j]);
+			}
+			
+			getch();
+			break;
 		case 3:
-			printf("Adios....");
+			printf("Opcion 3 - Ver Alumnos por Clasificacion de Notas \n");
 			break;	
+		case 4:
+			printf("Fin del Programa.");
+			
+			respuesta='n';
+			
+			break;	
+			
+		default :
+			printf("No existe la Opcion!");
+			getch();
+      } 
+	}    	
 	}
+    
 }
 
 // --> Main
